2246c2b226b54e5873ec64d38a82da6397d2558e
Takayuki Uchida <jacobitebeerlove@gmail.com>
2024-11-06 00:10:48 +0900

    fix: パッケージインストールディレクトリをデフォルトに変更

--- init.el
+++ init.el
@@ -14,7 +14,6 @@
 (eval-and-compile
   ;; Consts
   (defconst chpn/dir-jars      "~/.local/jar/")
-  (defconst chpn/dir-pkg-elpa  "~/.local/elisp/elpa/")
   (defconst chpn/dir-pkg-local "~/.local/elisp/local/")
 
   ;; Functions
@@ -80,8 +79,6 @@
                        ("melpa"        . "https://melpa.org/packages/")
                        ("melpa-stable" . "https://stable.melpa.org/packages/")
                        ("gnu"          . "https://elpa.gnu.org/packages/")))
-  (customize-set-variable 'package-user-dir chpn/dir-pkg-elpa)
-  (customize-set-variable 'package-gnupghome-dir (concat chpn/dir-pkg-elpa "gnupg/"))
   (customize-set-variable 'gnutls-algorithm-priority  "normal:-vers-tls1.3")
 
   (package-initialize)
