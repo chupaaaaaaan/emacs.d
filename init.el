99afd92d46c91c4544e2a8edbdedd35ce2a0c7e2
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-06-26 15:17:17 +0900

    テンプレートファイルのパスを変更

--- init.el
+++ init.el
@@ -886,13 +886,13 @@ https://github.com/ema2159/centaur-tabs#my-personal-configuration"
 
   ;; capture
   (org-capture-templates . `(("t" "task: 新規タスク" entry (file "agenda/inbox.org")
-                              ,(concat "%[" user-emacs-directory "capture-templates/inbox.org]")
+                              ,(concat "%[" user-emacs-directory "templates/org-capture/inbox.org]")
                               :empty-lines 1 :jump-to-captured nil)
                              ("s" "schedule: スケジュール" entry (file "agenda/inbox.org")
-                              ,(concat "%[" user-emacs-directory "capture-templates/schedule.org]")
+                              ,(concat "%[" user-emacs-directory "templates/org-capture/schedule.org]")
                               :empty-lines 1)
                              ("m" "memo: 新規メモ" plain (file chpn/today-memo-string-with-mkdir)
-                              ,(concat "%[" user-emacs-directory "capture-templates/memo.org]")
+                              ,(concat "%[" user-emacs-directory "templates/org-capture/memo.org]")
                               :empty-lines 1 :jump-to-captured 1 :unnarrowed nil)
                              ("l" "link: リンクを追加" item (clock)
                               "%A\n"
@@ -1008,7 +1008,7 @@ https://github.com/ema2159/centaur-tabs#my-personal-configuration"
     (save-some-buffers t))
 
   :init
-  (chpn/copy-directory-recursively (concat user-emacs-directory "agenda-templates/") (concat org-directory "agenda/"))
+  (chpn/copy-directory-recursively (concat user-emacs-directory "templates/org-agenda") (concat org-directory "agenda/"))
   :config
   (dolist (pattern org-agenda-files)
     (add-to-list 'recentf-exclude pattern))
