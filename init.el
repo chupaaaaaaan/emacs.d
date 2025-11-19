cac130b3cf8d15d51c4f2321047a14cf409a6ce6
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-11-20 00:56:34 +0900

    不要なconsult設定を削除

--- init.el
+++ init.el
@@ -660,10 +660,7 @@ https://github.com/ema2159/centaur-tabs#my-personal-configuration"
    ("M-s" . consult-history)
    ("M-r" . consult-history))
   (isearch-mode-map
-   ("M-e"   . consult-isearch-history)
-   ("M-s l" . consult-line)
-   ("M-s L" . consult-line-multi)
-   ("C-h"   . isearch-delete-char))
+   ("M-e" . consult-isearch-history))
   :hook
   (completion-list-mode-hook . consult-preview-at-point-mode))
 
